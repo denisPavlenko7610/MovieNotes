28551ea22dfc8b82d725fb165269ddd6caeed0e5
Denis Pavlenko <denispavlenko7610new@gmail.com>
2025-06-16 10:45:48 +0300

    combine activities, fix move to main activity after authorization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.RDragon.movienotes"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
