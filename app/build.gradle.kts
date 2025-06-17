87d6930b94ed1a2b1f3e146f28be1e91b3aa3bd4
Denis Pavlenko <denispavlenko7610new@gmail.com>
2025-06-17 23:59:22 +0300

    add sign

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,13 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
     id("com.google.gms.google-services")
-    id ("com.google.firebase.crashlytics")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
     namespace = "com.rdragon.movienotes"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.RDragon.movienotes"
         minSdk = 28
@@ -20,8 +21,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(property("RELEASE_STORE_FILE") as String)
+            storePassword = property("RELEASE_STORE_PASSWORD") as String
+            keyAlias = property("RELEASE_KEY_ALIAS") as String
+            keyPassword = property("RELEASE_KEY_PASSWORD") as String
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -29,13 +40,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
@@ -44,23 +58,27 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-    implementation (libs.material)
+    implementation(libs.material)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation (libs.androidx.constraintlayout)
-    implementation (libs.kotlin.stdlib)
-    implementation (libs.androidx.core.ktx)
-    implementation (libs.androidx.recyclerview)
-    implementation (libs.kotlinx.coroutines.core)
-    implementation (libs.kotlinx.coroutines.android)
-    implementation (libs.androidx.lifecycle.runtime.ktx)
-    implementation (libs.androidx.room.runtime)
-    implementation (libs.androidx.room.ktx)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
     ksp(libs.room.compiler)
-    implementation (libs.firebase.firestore.ktx)
-    implementation (libs.kotlinx.coroutines.play.services)
-    implementation ("com.google.firebase:firebase-auth:22.3.1")
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.kotlinx.coroutines.play.services)
+
+    // Firebase
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
-    implementation ("com.google.firebase:firebase-analytics")
-    implementation ("com.google.android.gms:play-services-auth:20.6.0")
+    implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
+
+    // Google Sign-In
+    implementation("com.google.android.gms:play-services-auth:20.6.0")
 }
